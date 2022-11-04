55a6194e8fd7392dd8e87493e1704809486f5b4c
midhilesh <chintu.midhilesh@gmail.com>
2022-11-04 06:48:52 +0530

    added new codes

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-first  = [11, 12, 13, 14, 15, 16]
-second = [71, 77, 89, 51, 90, 59]
-# Add two lists element-wise using zip() & sum()
-final_list = [abs(a - b) for a,b in zip(first, second)]
-print(final_list)
+s = 'abaccb'
+
+print(s.find('b'))
+
+
+
